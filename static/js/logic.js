d5c9da10a1b447bb8dbde4abf91fef66b46a30e8
khartmann5 <hartmann.kt@gmail.com>
2021-04-19 19:43:07 -0700

    saving work

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,28 +1,5 @@
-// Creating map object
-// const myMap = L.map("map", {
-//   center: [60, -152],
-//   zoom: 4
-// });
-
-// // Adding tile layer to the map
-// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "mapbox/streets-v11",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
 // Store API query variables
 const baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-// const date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
-// const complaint = "&complaint_type=Rodent";
-// const limit = "&$limit=10000";
-
-// Assemble API query URL
-// const url = baseURL + date + complaint + limit;
-// const url =baseURL;
 
 // Grab the data with d3
 d3.json(baseURL).then(data => {
@@ -37,10 +14,14 @@ function createFeatures(earthquakeData) {
 
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
+  // function onEachFeature(feature, layer) {
+  //   layer.bindPopup("<h3>" + feature.properties.title +
+  //     "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  // }
+
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.title +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
+      layer.bindPopup(`<p> Magnitude: ${feature.properties.mag}</p><p> Depth: ${feature.geometry.coordinates[2]}</p><p> Location: ${feature.properties.place}`);
+    }
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
@@ -66,13 +47,10 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes, mags) {
 
-  // Define streetmap and darkmap layers
-  const streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
+  const lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "light-v10",
     accessToken: API_KEY
   });
 
@@ -85,7 +63,7 @@ function createMap(earthquakes, mags) {
 
   // Define a baseMaps object to hold our base layers
   const baseMaps = {
-    "Street Map": streetmap,
+    "Light Map": lightmap,
     "Dark Map": darkmap
   };
 
@@ -100,8 +78,8 @@ function createMap(earthquakes, mags) {
     center: [
       37.09, -95.71
     ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
+    zoom: 2,
+    layers: [lightmap, earthquakes]
   });
 
   // Create a layer control
